72b2209e385b392a9c5271ff823c3cc6dd96bf3e
Maxim Bondar <zygelon@gmail.com>
2020-10-07 10:33:20 +0300

    procedural frontend update

--- FunnyEnglish/views.py
+++ FunnyEnglish/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Post 1',
+        'content': 'First post content',
+        'date_posted' : '07 September, 2020'
+    },
+    {
+        'author': 'Jane',
+        'title': 'Post 2',
+        'content': 'Second post content',
+        'date_posted' : '08 September, 2020'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Funny English Home</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request,'FunnyEnglish/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>Funny About</h1>')
+    return render(request,'FunnyEnglish/about.html')
